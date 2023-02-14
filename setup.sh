ffd0f334367742f07cbe2f4142ae32515d0e0d69
deepak.dhull <deepal.dhull@verse.in>
2023-02-14 23:16:42 +0530

    WordTokenizer added.

---
+++ setup.sh
@@ -0,0 +1,4 @@
+
+pip install -r requirements.txt
+# download nltk all packages, models and corpus
+python -m nltk.downloader all
--- transformer/tokenizer.py
+++ transformer/tokenizer.py
@@ -1,7 +1,23 @@
 import string
+from abc import ABC, abstractmethod
+from typing import *
 
 import numpy as np
-import pandas as pd
+from tqdm import tqdm
+from nltk.tokenize import word_tokenize
+
+class Tokenizer(ABC):
+    @abstractmethod
+    def fit(self,x):
+        pass
+
+    @abstractmethod
+    def encode(self, sen):
+        pass
+
+    @abstractmethod
+    def decode(self, tokens):
+        pass
 
 class CharTokenizer:
     from tqdm import tqdm
@@ -56,15 +72,111 @@ class CharTokenizer:
         return "".join([self.v2k[t] for t in tokens[mask]])
 
 
+class WordTokenizer(Tokenizer):
+    def __init__(self,
+                 max_length,
+                 filter = lambda x: x not in string.punctuation,
+                 **kwargs
+    ):
+        """WordTokenizer
+
+        Args:
+            max_length (int): maximum tokens in a sentence.
+            filter (lambda, optional): lambda|function which filter the tokens. Defaults to lambda x: x not in string.punctuation.
+        """
+        self.filter = filter
+        self.max_length = max_length
+        self.tokens = set()
+        self.SOT = "<SOT>"
+        self.EOT = "<EOT>"
+
+    @staticmethod
+    def tokenize(sentence: str) -> List:
+        return word_tokenize(sentence)
+
+    def preprocess(self, sentence:str) -> List[str]:
+        sentence = sentence.lower().strip()
+        tokens = WordTokenizer.tokenize(sentence)
+        return set(filter(self.filter, tokens))
+    
+    def fit(self, docs: List[str], enable_docs_persist=False):
+        
+        print(f"Tokenizing the docs. No of docs {len(docs)}.")
+        for sentence in tqdm(docs, total=len(docs)):
+            token = self.preprocess(sentence)
+            self.tokens = self.tokens.union(token)
+        
+        self.tokens = list(self.tokens)
+        print(f"The number of tokens are {len(self.tokens)}")
+        self.tokens.insert(0,self.SOT)
+        self.tokens.insert(0,self.EOT)
+
+        self.k2w = {
+            i+1:w for i, w in enumerate(self.tokens)
+        }
+
+        self.w2k = {
+            v:k for k,v in self.k2w.items()
+        }
+        if enable_docs_persist:
+            self.vec, self.mask = self.encode(docs)
+
+
+
+    def encode(self, docs:List[str]) -> Tuple[np.ndarray, np.ndarray]:
+        sen_li = []
+        mask_li = []
+        for sentence in docs:
+            _x = [self.w2k[w] for w in self.preprocess(sentence)]
+            _x = _x[:self.max_length]
+            sen_len = len(_x)
+            if self.max_length > sen_len:
+                padding = self.max_length - sen_len
+                _x = _x + [-1 for _ in range(padding)]
+            
+            _mask = [1 for _ in range(sen_len)] + [0 for _ in range(self.max_length - sen_len)]
+            sen_li.append(_x)
+            mask_li.append(_mask)
+        
+        return np.array(sen_li), np.array(mask_li)
+
+    def decode(self, tokens:Union[List[List[int]], np.ndarray]) -> List[str]:
+        
+        def _decode(_tokens: List[int]):
+            return " ".join([self.k2w[token] for token in _tokens if token != 0])
+        
+        res_li = []
+        for _tokens in tokens:
+            res_li.append(_decode(_tokens))
+        return res_li
+
+
+
 if __name__ == '__main__':
-	tokenizer = CharTokenizer(max_len=120)
-	ds = pd.read_csv("D:\Research Lab\datasets\language-dataset\hi-en-text/hindi_english_parallel.csv")
-	ds['wc'] = ds['english'].map(lambda x: len(str(x).split(" ")))
-	from utils import isalphanum
+    import pandas as pd
+    ### CharTokenizer
 
-	ds['isalphanum'] = ds['english'].map(lambda x: isalphanum(str(x)))
-	dataset = ds[(ds['wc']>10) & (ds['isalphanum'])].reset_index(drop=True)
+	# tokenizer = CharTokenizer(max_len=120)
+	# ds = pd.read_csv("D:\Research Lab\datasets\language-dataset\hi-en-text/hindi_english_parallel.csv")
+	# ds['wc'] = ds['english'].map(lambda x: len(str(x).split(" ")))
+	# from utils import isalphanum
 
-	tokenizer.fit(dataset['english'])
-	print(tokenizer.item[0])
-	print(tokenizer.decode(tokenizer.item[0]))
+	# ds['isalphanum'] = ds['english'].map(lambda x: isalphanum(str(x)))
+	# dataset = ds[(ds['wc']>10) & (ds['isalphanum'])].reset_index(drop=True)
+
+	# tokenizer.fit(dataset['english'])
+	# print(tokenizer.item[0])
+	# print(tokenizer.decode(tokenizer.item[0]))
+
+    ### WordTokenizer
+    df = pd.read_csv("data/wiki-data/cnn_dailymail/validation.csv")
+    
+    tokenizer = WordTokenizer(12)
+    tokenizer.fit(df['highlights'].to_list()[:20])
+    
+    vec, mask = tokenizer.encode(df['highlights'].to_list()[:10])
+    
+    print(vec.shape, mask.shape)
+    print(vec[0])
+    print(mask[0])
+    print(tokenizer.decode(vec[:2]))
